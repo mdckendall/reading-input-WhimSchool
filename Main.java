ac69475d233dfef54d1a498dea5e48b84a77e178
WhimSchool <113545861+WhimSchool@users.noreply.github.com>
2023-06-26 22:59:34 +0000

    First attempt, returns just the string length as a println

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
     System.out.println("Enter a word:");
-    
+    String word = input.nextLine();
+    System.out.println("" + word.length());
     //Your code will return the length of the entered String
+
+    input.close();
   }
 }
